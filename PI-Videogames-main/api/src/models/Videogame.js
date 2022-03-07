ed53a6c6ebfb7af269526350c3e185c040b3d60b
Leonardo Sanchez <leonardosanchez119@gmail.com>
2022-03-07 14:00:58 -0300

    feat:add content to github

--- PI-Videogames-main/api/src/models/Videogame.js
+++ PI-Videogames-main/api/src/models/Videogame.js
@@ -4,9 +4,29 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id:{
+      allowNull: false,
+      type:DataTypes.UUID,
+      primaryKey:true,
+      defaultValue: DataTypes.UUIDV4
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description:{
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    releaseDate:{
+      type:DataTypes.STRING
+    },
+    rating:{
+      type:DataTypes.STRING
+    },
+    plataforms:{
+      type:DataTypes.STRING,
+      allowNull:false
+    }
   });
 };
