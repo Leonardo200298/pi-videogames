6c8acbb1de51f8f0f6838a36af5d3dd9643edaf9
Leonardo Sanchez <leonardosanchez119@gmail.com>
2022-04-04 13:13:16 -0300

    add backend modified to github

--- PI-Videogames-main/api/src/controllersFunctions/apiFunctions.js
+++ PI-Videogames-main/api/src/controllersFunctions/apiFunctions.js
@@ -1,28 +1,25 @@
 const axios = require('axios');
 const { response } = require('../app');
+const { get } = require('../routes');
 const API = 'https://api.rawg.io/api/games?key='
 const APIKEY = '8df31d27f3724043a2e647ffc07e80f5'
 
 async function getVideogamesByApi() {
-    var videogamesData
-    const elementsOfApi =await axios.get(API + APIKEY)
-    await Promise.all([elementsOfApi])
-        .then((respuesta) => {
-            const [videogamesOfApi] = respuesta
-            videogamesData = videogamesOfApi.data.results.map((data) => {
+    const videogamesData = await axios.get(API + APIKEY)
+    /* console.log(videogamesData) */
+    const apiData = await videogamesData.data.results.map((data)=>{
+        return {
 
-                return ({
-                    name: data.name,
-                    img: data.background_image,
-                    id: data.id,
-                    rating: data.rating,
-                    description: data.slug,
-                    genre: data.genres.map((info) => { return { name: info.name} }),
-                    platforms: data.platforms.map((info) => { return { name: info.platform.name, id: info.platform.id } })
-                })
-            })
-        })
-        return videogamesData;
+             name: data.name,
+             img: data.background_image,
+             id: data.id,
+             rating: data.rating,
+             description: data.slug,
+             genre: data.genres.map((info) => { return { name: info.name} }),
+             platforms: data.platforms.map((info) => { return { name: info.platform.name, id: info.platform.id } })
+        }
+    })
+    return apiData 
 }
 
 //solo por hoy vamos a traer 20 para probar la api
